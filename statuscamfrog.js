cbeb9dbd8871f77e4c572d67ff5b42586ade8024
Cafe Camfrog <cafecamfrog0@gmail.com>
2018-03-26 08:04:16 +0700

    Create statuscamfrog.js

---
+++ statuscamfrog.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+<!--
+camfrog_nick = '';
+camfrog_domain = 'camfrog.com';
+//-->
+</script>
+<script type="text/javascript" src="https://www.camfrog.com/id/js/get_status_picture.js">
+</script>
